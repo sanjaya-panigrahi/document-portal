6161b48232b95924ae6f287505c278213707a66d
Sanjaya Panigrahi <47269667+sanjaya-panigrahi@users.noreply.github.com>
2026-07-23 17:13:22 +0530

    update folder further

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup( 
+    name='docportal',
+    version='0.1',
+    packages=find_packages(),
+    author='Sanjaya Panigrahi',
+    author_email='sanjayp.panigrahi@gmail.com',
+)
